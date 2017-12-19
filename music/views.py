fd5743a48cc45bc18fb41dc1295a8e809dbb7fdb
Alex Persaud <alex_persaud01@yahoo.com>
2017-12-19 14:34:01 -0500

    Create index template

--- music/views.py
+++ music/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album
+from django.template import loader
 
 # Create your views here.
 def index(request):
-  return HttpResponse("This is the music homepage.")
+  all_albums = Album.objects.all()
+  template = loader.get_template("music/index.html")
+  context = {
+    'all_albums': all_albums,
+  }
+
+  return HttpResponse(template.render(context, request))
+
 
 def detail(request, album_id):
   return HttpResponse("This album number is " + str(album_id))
