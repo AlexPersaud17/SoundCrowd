eeab2f06707431458c7131c7a18237a6014c5fc1
Alex Persaud <alex_persaud01@yahoo.com>
2017-12-19 14:12:52 -0500

    Create view for album ids

--- music/views.py
+++ music/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
   return HttpResponse("This is the music homepage.")
+
+def detail(request, album_id):
+  return HttpResponse("This album number is " + str(album_id))
