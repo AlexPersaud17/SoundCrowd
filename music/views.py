f6f995956494bafabaa9ebdb284113d94a260ad8
Alex Persaud <alex_persaud01@yahoo.com>
2017-12-20 11:48:11 -0500

    Create post method for favoriting songs

--- music/views.py
+++ music/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Album
+from .models import Album, Song
 
 def index(request):
   all_albums = Album.objects.all()
@@ -11,5 +11,16 @@ def detail(request, album_id):
   return render(request, 'music/detail.html', {'album': album})
 
 
-def favorite(request):
-  pass
+def favorite(request, album_id):
+  album = get_object_or_404(Album, id=album_id)
+  try:
+    selected_song = album.song_set.get(id=request.POST['song'])
+  except (KeyError, Song.DoesNotExist):
+    return render(request, 'music/detail.html', {
+  'album': album,
+  'error_message': 'You did not select a valid song',
+  })
+  else:
+    selected_song.is_favorite = True
+    selected_song.save()
+    return render(request, 'music/detail.html', {'album': album})
