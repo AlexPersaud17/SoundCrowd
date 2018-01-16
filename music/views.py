86ac0e5256a02113df002835b7a592666645f737
Alex Persaud <alex_persaud01@yahoo.com>
2018-01-16 13:56:58 -0500

    Create user form class

--- music/views.py
+++ music/views.py
@@ -1,7 +1,11 @@
-from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views import generic
+from django.views.generic import View
 from .models import Album
+from .forms import UserForm
 
 class IndexView(generic.ListView):
   template_name = 'music/index.html'
